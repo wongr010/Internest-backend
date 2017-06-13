4436c098ad3a265d128b46b3964de1ed1d8aefd0
Rosalyn <r928wong@gmail.com>
2017-06-13 10:37:26 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,18 @@
+//Creating a http server at http://127.0.0.1:8081/
+//import requried module
+var http=require("http");
+
+//create server instance 
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081); //use port 8081
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
