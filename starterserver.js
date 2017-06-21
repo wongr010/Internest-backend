6aedfa37de66264857bd469f04d079fa5d7c45c8
Rosalyn <r928wong@gmail.com>
2017-06-20 21:54:08 -0400

    Creating a local server

---
+++ starterserver.js
@@ -0,0 +1,25 @@
+
+var http=require("http"); //require the http module
+
+var server=http.createServer(function(request, response){ 
+	//createServer creates an instance of a local server, named 'server'
+	//function takes a callback function as an argument
+	//callback function is a function that is called every time a client sends a request to the server
+
+	response.writeHead(200, {"Content-Type": "text/html"}); //http status code
+	response.write('<!DOCTYPE html><html lang="en"><head>');
+    response.write('<meta charset="utf-8">');
+	
+	response.write("<title>Hello World Page</title>");
+	response.write("</head>");
+	response.write("<body>");
+	response.write("Hello World!");
+	response.write("</body>");
+	response.write("</html>");
+	response.end();
+
+
+})
+
+server.listen(8080); //bind server to port 80, listen for connections
+console.log("The end"); //access using localhost:8080
--- streams.js
+++ streams.js
@@ -20,4 +20,27 @@ readerStream.on('error', function(err){ //handling an error
    console.log(err.stack);
 });
 
+//writing to a stream (text file)
+
+
+var data = "I don't know what it says about interns";
+
+// Create a writable stream
+var writerStream = fs.createWriteStream('outpractice.txt');
+
+// Write the data to stream with encoding to be utf8
+writerStream.write(data,'UTF8');
+
+// Mark the end of file
+writerStream.end();
+
+// Handle stream events --> finish, and error
+writerStream.on('finish', function() {
+    console.log("Write completed.");
+});
+
+writerStream.on('error', function(err){
+   console.log(err.stack);
+});
+
 console.log("Program Ended");
