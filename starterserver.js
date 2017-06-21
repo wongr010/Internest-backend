282e0d01606e555544b2525d73dd913cc58960d9
Rosalyn <r928wong@gmail.com>
2017-06-20 23:29:41 -0400

    Opening an html file when running node.js file

--- starterserver.js
+++ starterserver.js
@@ -1,13 +1,19 @@
 
 var http=require("http"); //require the http module
+var fs=require("fs");
 
-var server=http.createServer(function(request, response){ 
+fs.readFile('index.html', function (err, html) {
+	if (err) {
+		throw err; 
+	} 
+
+	var server=http.createServer(function(request, response){ 
 	//createServer creates an instance of a local server, named 'server'
 	//function takes a callback function as an argument
 	//callback function is a function that is called every time a client sends a request to the server
 
 	response.writeHead(200, {"Content-Type": "text/html"}); //http status code
-	response.write('<!DOCTYPE html><html lang="en"><head>');
+	/*response.write('<!DOCTYPE html><html lang="en"><head>');
     response.write('<meta charset="utf-8">');
 	
 	response.write("<title>Hello World Page</title>");
@@ -15,11 +21,12 @@ var server=http.createServer(function(request, response){
 	response.write("<body>");
 	response.write("Hello World!");
 	response.write("</body>");
-	response.write("</html>");
+	response.write("</html>");*/
+
+	response.write(html);
 	response.end();
 
 
-})
-
-server.listen(8080); //bind server to port 80, listen for connections
+}).listen(8080); //bind server to port 80, listen for connections
+});
 console.log("The end"); //access using localhost:8080
